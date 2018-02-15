b9997544cafbf1f6c7a7b97ba5ad6ce6fa7dd106
ARamosCortes <36413085+ARamosCortes@users.noreply.github.com>
2018-02-15 11:06:37 -0500

    Add files via upload

---
+++ 04/05/CommentsAndSlashes.java
@@ -0,0 +1,13 @@
+public class CommentsAndSlashes {
+    public static void main(String[] args) {
+        // A comment, this is so you can diddily doo your hoo.
+        // This shuld be ignored I guess.
+
+        System.out.println("I smell like beef."); // this should also be ignored.
+
+        // If I do a thingy in front of another thingy then it'll prevent the thingy from printing.
+        // System.out.println("This shouldn't work.");
+
+        System.out.println("$5 if I get this to work on the first try");
+    }
+}

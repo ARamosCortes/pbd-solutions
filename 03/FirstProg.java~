d362493c5f4ab325472fa6979faeae46b4ef3a54
ARamosCortes <36413085+ARamosCortes@users.noreply.github.com>
2018-02-15 10:41:27 -0500

    Add files via upload

---
+++ 03/FirstProg.java~
@@ -0,0 +1,7 @@
+public class FirstProg
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "public ststic void main( String[] args )" );
+    }
+}
---
+++ 04/GoodFirstProgram.java
@@ -0,0 +1,13 @@
+public class GoodFirstProgram
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello again!" );
+        System.out.println( "This is fun!" );
+        System.out.println( "Please send help the North Koreans have me captured." );
+        System.out.println( "I like typing like this!" );
+        System.out.println( "Please help me they've had me here for months watching propaganda videos." );
+        System.out.println( "This is very entertaining!" );
+    }
+}

4b0ad4b8aff822815c8f7b704f4bb0d34a79ad1e
ARamosCortes <36413085+ARamosCortes@users.noreply.github.com>
2018-02-15 10:47:42 -0500

    Update FirstProg.java

--- 03/FirstProg.java
+++ 03/FirstProg.java
@@ -1,7 +1,5 @@
-public class FirstProg
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "public ststic void main( String[] args )" );
+public class FirstProg {
+    public static void main(String[] args) {
+        System.out.println("public ststic void main( String[] args )");
     }
 }
